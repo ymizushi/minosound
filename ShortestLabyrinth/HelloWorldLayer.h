3c44b18467cac3753c612ddb9fcb5050f3bb3080
ymizushi <mizushi@gmail.com>
2013-04-02 01:51:08 +0900

    Initial Commit

---
+++ ShortestLabyrinth/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  ShortestLabyrinth
+//
+//  Created by 水島 雄太 on 2013/04/02.
+//  Copyright 水島 雄太 2013年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ ShortestLabyrinth/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ShortestLabyrinth
+//
+//  Created by 水島 雄太 on 2013/04/02.
+//  Copyright 水島 雄太 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
