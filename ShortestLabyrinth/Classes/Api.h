8a55100fca4c677c7e1b232cf3863410de8fa22f
ymizushi <mizushi@gmail.com>
2013-08-18 19:13:20 +0900

    add api class

---
+++ ShortestLabyrinth/Classes/Api.h
@@ -0,0 +1,13 @@
+//
+//  Api.h
+//  minosound
+//
+//  Created by Yuta Mizushima on 2013/08/18.
+//  Copyright (c) 2013年 水島 雄太. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Api : NSObject
+
+@end
---
+++ ShortestLabyrinth/Classes/UserApi.h
@@ -0,0 +1,14 @@
+//
+//  UserApi.h
+//  minosound
+//
+//  Created by Yuta Mizushima on 2013/08/18.
+//  Copyright (c) 2013年 水島 雄太. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Api.h"
+
+@interface UserApi : Api
+
+@end
