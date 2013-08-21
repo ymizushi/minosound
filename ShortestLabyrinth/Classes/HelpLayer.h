665f1470f6e906101481e119df060d7881300472
ymizushi <mizushi@gmail.com>
2013-08-22 02:34:41 +0900

    add HelpLayer

---
+++ ShortestLabyrinth/Classes/HelpLayer.h
@@ -0,0 +1,13 @@
+//
+//  HelpLayer.h
+//  minosound
+//
+//  Created by Yuta Mizushima on 2013/08/22.
+//  Copyright (c) 2013年 水島 雄太. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelpLayer : NSObject
+
+@end
---
+++ ShortestLabyrinth/Classes/HelpLayer.m
@@ -0,0 +1,13 @@
+//
+//  HelpLayer.m
+//  minosound
+//
+//  Created by Yuta Mizushima on 2013/08/22.
+//  Copyright (c) 2013年 水島 雄太. All rights reserved.
+//
+
+#import "HelpLayer.h"
+
+@implementation HelpLayer
+
+@end
