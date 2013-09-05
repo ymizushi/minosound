820cafbeb6e3637159e47533dd87d964f8a6a571
ymizushi <mizushi@gmail.com>
2013-09-06 02:49:00 +0900

    user user api

--- ShortestLabyrinth/Classes/UserApi.m
+++ ShortestLabyrinth/Classes/UserApi.m
@@ -22,4 +22,28 @@
     return jsonObject;
 }
 
++ (NSDictionary*)update:(NSInteger)userId :(NSDictionary*)params {
+    NSString* userIdStr = [NSString stringWithFormat:@"%d", userId];
+    NSString *apiUrlJson = [UrlUtil genUrl:@[@"/users/", userIdStr]];
+    NSURL *httpDataUrl2 = [NSURL URLWithString:apiUrlJson];
+    
+    NSString *postString = [NSString stringWithFormat:@"id=%@&passwd=%@", @"122", @"hogehog"];
+    NSData *post = [postString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    
+    NSMutableURLRequest *request =  [NSMutableURLRequest requestWithURL:httpDataUrl2 cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"content-type"];
+    [request setHTTPBody:post];
+    
+    NSURLResponse* response;
+    NSError* error = nil;
+    NSData* result = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    
+    NSString* resultString = [[NSString alloc] initWithData:result encoding:NSUTF8StringEncoding];
+    NSLog(@"%@", resultString);
+    return @{};
+    
+}
+
 @end
